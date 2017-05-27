a56e563c1a41d863297aa53aa5a3ce555ef75390
MinaFujisawa <fujimina37@gmail.com>
2017-05-27 09:57:12 -0700

    fixing

--- app/src/main/java/com/example/flashnumber/MainActivity.java
+++ app/src/main/java/com/example/flashnumber/MainActivity.java
@@ -16,14 +16,16 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Random random = new Random();
-    private int clickNum = 4;
-    private TextView mCountDownTextView;
-    private int[] setNumList = new int[clickNum];
-    private int[] setNumListOrdered = new int[clickNum];
-    private int clicked = 0;
     private static final int MAX_NUM = 15;
+    private static final int MIN_CLICK_NUM = 4;
+    private int clickNum = MIN_CLICK_NUM;
+    private TextView mCountDownTextView;
+    private int[] setNumList;
+    private int[] setNumListOrdered;
+    private int clicked = 0;
 
-    private ArrayList<Button> btnList = new ArrayList<>();
+
+    private ArrayList<Button> btnList = new ArrayList<>();;
 
     @Override
     public void onClick(View v) {
@@ -33,11 +35,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //get btn num index
         int length = String.valueOf(b).length();
         String btnIndexString = String.valueOf(b);
-        int btnIndex = Integer.parseInt(String.valueOf(b).substring(length-2, length-1))-1;
+        int btnIndex = Integer.parseInt(String.valueOf(b).substring(length - 2, length - 1)) - 1;
 
-        if(clicked+1 == clickNum){
+        //check the answers
+        if (clicked + 1 == clickNum) {
             b.setText(String.valueOf(setNumListOrdered[clicked]));
             clickNum++;
+            loadActivity();
         } else {
             if (setNumList[btnIndex] == (setNumListOrdered[clicked])) {
                 b.setText(String.valueOf(setNumListOrdered[clicked]));
@@ -53,25 +57,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //switch layouts
-        switch (clickNum) {
-            case 4:
-                setContentView(R.layout.num_four);
-                clickNum = 4;
-                break;
-            case 5:
-                setContentView(R.layout.num_five);
-                clickNum = 5;
-                break;
-        }
-
-        // add Button to the arrayList
         btnList.add((Button) findViewById(R.id.btn1));
         btnList.add((Button) findViewById(R.id.btn2));
         btnList.add((Button) findViewById(R.id.btn3));
         btnList.add((Button) findViewById(R.id.btn4));
         btnList.add((Button) findViewById(R.id.btn5));
+
+        // add Button to the arrayList
+
+        loadActivity();
+    }
+
+    private void loadActivity() {
+
+        //create arrays every time changed the layout
+        setNumList = new int[clickNum];
+        setNumListOrdered = new int[clickNum];
+
+
+
+        btnList.get(0).setText("HA");
+
+        // add click listeners for the above buttons
+//        for (int i = 0; i < clickNum; i++) {
+//            btnList.get(i).setOnClickListener(this);
+//        }
+
+
+        //switch layouts
+        switch (clickNum) {
+            case 4:
+                setContentView(R.layout.num_four);
+                break;
+            case 5:
+                setContentView(R.layout.num_five);
+                break;
+        }
+
 
         //Count down
         mCountDownTextView = (TextView) findViewById(R.id.countDown);
@@ -85,7 +107,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mCountDownTextView.setText("");
                 setNum(clickNum);
                 //invisible number
-                new CountDownTimer(3000, 1000) {
+                new CountDownTimer(2000, 1000) {
                     public void onTick(long millisUntilFinished) {
                     }
 
@@ -99,30 +121,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }.start();
 
-        // add click listeners for all buttons
-        for (int i = 0; i < clickNum; i++) {
-            btnList.get(i).setOnClickListener(this);
-        }
 
 
     }
+
+
+
 
     public void setNum(int clickNum) {
-        boolean num[] = new boolean[MAX_NUM];
-        int i = 0;
-        while (i < clickNum) {
-            int p = random.nextInt(MAX_NUM);
-            if (num[p] == false) {
-                btnList.get(i).setText(String.valueOf(p));
-                setNumListOrdered[i] = p;
-                setNumList[i] = p;
-                num[p] = true;
-                i++;
-            }
-        }
-        Arrays.sort(setNumListOrdered);
-
+//        boolean num[] = new boolean[MAX_NUM];
+//        int i = 0;
+//        while (i < clickNum) {
+//            int p = random.nextInt(MAX_NUM);
+//            if (num[p] == false) {
+//                btnList.get(i).setText(String.valueOf(p));
+//                setNumListOrdered[i] = p;
+//                setNumList[i] = p;
+//                num[p] = true;
+//                i++;
+//            }
+//        }
+//        Arrays.sort(setNumListOrdered);
     }
+
+    private View.OnClickListener ReloadActivity = new View.OnClickListener() {
+        public void onClick(View v) {
+            loadActivity();
+        }
+    };
 
 
 }
